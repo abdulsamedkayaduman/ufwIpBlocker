d1e86a3181d31642fea9deb0fc1ebab7f9e19a82
abdulsamed <abdulsamedkayaduman@gmail.com>
2019-11-08 12:32:13 +0000

    Düzenlemeler yapıldı

--- ipBlocker.sh
+++ ipBlocker.sh
@@ -9,28 +9,42 @@ i=0
 sleep 1
 while IFS= read -r line
 do
-echo "whilee"
-
+ 
 userExist=$(echo "$line" | awk '{print $10}')
-
+#if user exist in machine
 if [ "$userExist" = "from" ]; then
 user=$(echo "$line" | awk '{print $9}')
 ip=$(echo "$line" | awk '{print $11}')
 
 sudo ufw deny from $ip to any
 echo "Blocked ip $ip for user $user" >> blockList.txt
+echo "$ip" >> blockIps.txt
+
+
 
 #else
 
 fi
 
-echo "invalid user last num $i"
-echo "$userExist"
-echo "$line"
+#if user is unknown
+
+if [ "$userExist" = "user" ]; then
+user=$(echo "$line" | awk '{print $11}')
+ip=$(echo "$line" | awk '{print $13}')
+
+sudo ufw deny from $ip to any
+echo "Blocked ip $ip for user $user" >> blockList.txt
+echo "$ip" >> blockIps.txt
+
+#else
+
+fi
+
+echo "Line Num $i"
 
 i=$((i+1))
-
- 
+echo "INFO :: Blocked ip $ip for user $user"
 done < "$authlog"
 
 echo "Done Ok Done"
+
