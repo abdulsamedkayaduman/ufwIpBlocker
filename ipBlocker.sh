65a5f29b57d1ffb3e69e46498d4dbc22e9e9198e
abdulsamed <abdulsamedkayaduman@gmail.com>
2019-11-10 21:04:38 +0000

    dosya düzenleri düzenlendi

--- ipBlocker.sh
+++ ipBlocker.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 authlogPath="/var/log/auth.log"
-cp $authlogPath auth.log
-sudo chmod 777 auth.log
-authlog="auth.log"
-grep "Failed password for" $authlog | grep -v "COMMAND=" > authGrep.log
-authlog="authGrep.log"
+workPath="work"
+cp $authlogPath $workPath/auth.txt
+sudo chmod 777 $workPath/auth.txt
+authlog="$workPath/auth.txt"
+grep "Failed password for" $authlog | grep -v "COMMAND=" > $workPath/authGrep.txt
+authlog="$workPath/authGrep.txt"
 i=0
+spinner="/|\\-/|\\-"
 sleep 1
 while IFS= read -r line
 do
@@ -17,8 +19,8 @@ user=$(echo "$line" | awk '{print $9}')
 ip=$(echo "$line" | awk '{print $11}')
 
 sudo ufw insert 1 deny from $ip to any
-echo "Blocked ip $ip for user $user" >> blockList.txt
-echo "$ip" >> blockIps.txt
+echo "Blocked ip $ip for user $user" >> $workPath/blockList.txt
+echo "$ip" >> $workPath/blockIps.txt
 
 
 
@@ -33,17 +35,21 @@ user=$(echo "$line" | awk '{print $11}')
 ip=$(echo "$line" | awk '{print $13}')
 
 sudo ufw insert 1 deny from $ip to any
-echo "Blocked ip $ip for user $user" >> blockList.txt
-echo "$ip" >> blockIps.txt
+echo "Blocked ip $ip for user $user" >> $workPath/blockList.txt
+echo "$ip" >> $workPath/blockIps.txt
 
 #else
 
 fi
 
-echo "Line Num $i"
+#echo "Line Num $i"
 
 i=$((i+1))
-echo "INFO :: Blocked ip $ip for user $user"
+#echo "INFO :: Blocked ip $ip for user $user"
+
+echo -n "${spinner:$n:1}"
+
+sleep 1 
 done < "$authlog"
 
 echo "Done Ok Done"
