68168f76a5aed99028db6f141b0ca200e158c54c
abdulsamed <abdulsamedkayaduman@gmail.com>
2019-11-19 22:20:08 +0000

    satır degişikliği baz alındı

--- ipBlocker.sh
+++ ipBlocker.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 authlogPath="/var/log/auth.log"
-workPath="work"
-cp $authlogPath $workPath/auth.txt
+workPath="/home/abdulsamed/ipBlocker/work"
+authlogLastLineNum=$( sudo wc -l $authlogPath |  awk '{print $1}' )
+lastLineNum=$( tail -1 $workPath/lastLineNum.txt)
+
+sudo sed -n "$lastLineNum,\$p" $authlogPath > $workPath/auth.txt
 sudo chmod 777 $workPath/auth.txt
 authlog="$workPath/auth.txt"
-grep "Failed password for" $authlog | grep -v "COMMAND=" > $workPath/authGrep.txt
+echo "Grep authlog started...."
+grep "Failed password for" $authlog | grep -v "COMMAND=" | cut -d " " -f 6- | sort | uniq > $workPath/authGrep.txt
 authlog="$workPath/authGrep.txt"
 i=0
+echo $authlogLastLineNum > $workPath/lastLineNum.txt 
+echo "Grep authlog ended...."
+echo "Changed Line Size = $(($authlogLastLineNum-$lastLineNum))"
 
+echo "Cleaning files started...."
 > $workPath/ufwOut.txt
 > $workPath/blockList.txt
 > $workPath/listUserIp.txt
 > $workPath/listIp.txt
+> $workPath/blockedIPs.txt 
+echo "Cleaning files ended...."
 sleep 1
 
+echo "Blocaked IPs value "
+sudo ufw status | wc -l
+echo "------------"
+sudo ufw status |  grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" > $workPath/blockedIPs.txt 
 lineNum=$( wc -l $authlog | awk '{print $1}')
 echo $lineNum
 while IFS= read -r line
 do
  
-userExist=$(echo "$line" | awk '{print $10}')
+userExist=$(echo "$line" | awk '{print $5}')
 #if user exist in machine
 if [ "$userExist" = "from" ]; then
-user=$(echo "$line" | awk '{print $9}')
-ip=$(echo "$line" | awk '{print $11}')
+user=$(echo "$line" | awk '{print $4}')
+ip=$(echo "$line" | awk '{print $6}')
 
 #sudo ufw insert 1 deny from $ip to any >> $workPath/ufwOut.txt
 echo "$ip,$user" >> $workPath/listUserIp.txt
@@ -31,8 +45,8 @@ echo "$ip" >> $workPath/listIp.txt
 fi
 
 if [ "$userExist" = "user" ]; then
-user=$(echo "$line" | awk '{print $11}')
-ip=$(echo "$line" | awk '{print $13}')
+user=$(echo "$line" | awk '{print $6}')
+ip=$(echo "$line" | awk '{print $8}')
 
 #sudo ufw insert 1 deny from $ip to any >> $workPath/ufwOut.txt
 echo "$ip,$user" >> $workPath/listUserIp.txt
@@ -50,18 +64,26 @@ lineNum=0
 i=0
 
 sort $workPath/listIp.txt | uniq  > $workPath/listIpUniq.txt
+sort $workPath/blockedIPs.txt > $workPath/blockedIPsTMP.txt
+mv $workPath/blockedIPsTMP.txt $workPath/blockedIPs.txt
+sort $workPath/listIpUniq.txt > $workPath/listIpUniqTMP.txt
+mv $workPath/listIpUniqTMP.txt $workPath/listIpUniq.txt
 
+comm -2 -3 $workPath/listIpUniq.txt  $workPath/blockedIPs.txt | grep -v from | grep -v port > $workPath/diffIpList.txt
 
-
-lineNum=$( wc -l $workPath/listIpUniq.txt | awk '{print $1}')
+lineNum=$( wc -l $workPath/diffIpList.txt | awk '{print $1}')
 echo "\n"
+
 while IFS= read -r line
 do
 	sudo ufw insert 1 deny from $line to any  >> $workPath/ufwOut.txt
 	i=$((i+1))
     result=$(((100*$i)/$lineNum))
 
-    echo "\b\rPercentage $result%. Blocked Ip for $line "
-done < "$workPath/listIpUniq.txt"
-	
+    echo "\b\b\r Percentage $result%. Blocked Ip for $line ...."
+done < "$workPath/diffIpList.txt"
+
+echo "Blocaked IPs value "
+sudo ufw status | wc -l
+echo "------------"
 echo "Done Ok Done"
