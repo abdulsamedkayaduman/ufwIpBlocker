c367bd2e186848afec5b51c6343d4b7b4e657de8
abdulsamed <abdulsamedkayaduman@gmail.com>
2020-01-16 21:43:12 +0000

    Düzeltmeler yapıldı

--- ipBlocker.sh
+++ ipBlocker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-authlogPath="/var/log/auth.log"
-workPath="/home/abdulsamed/ipBlocker/work"
-authlogLastLineNum=$( sudo wc -l $authlogPath |  awk '{print $1}' )
-lastLineNum=$( tail -1 $workPath/lastLineNum.txt)
-
-sudo sed -n "$lastLineNum,\$p" $authlogPath > $workPath/auth.txt
+authlogPath="/var/log/auth.log" #ana log dizini 
+workPath="/home/abdulsamed/ipBlocker/work" #dosya isleme dizini
+authlogLastLineNum=$( sudo wc -l $authlogPath |  awk '{print $1}' ) #authlogun satir sayisi
+lastLineNum=$( tail -1 $workPath/lastLineNum.txt) # bir onceki taramada satir sayisi
+echo "Last readed last line num = $lastLineNum"
+sudo sed -n "$lastLineNum,\$p" $authlogPath > $workPath/auth.txt # kalan satırdan baslatma icin
 sudo chmod 777 $workPath/auth.txt
 authlog="$workPath/auth.txt"
 echo "Grep authlog started...."
@@ -13,7 +13,17 @@ authlog="$workPath/authGrep.txt"
 i=0
 echo $authlogLastLineNum > $workPath/lastLineNum.txt 
 echo "Grep authlog ended...."
-echo "Changed Line Size = $(($authlogLastLineNum-$lastLineNum))"
+diffLine=$(($authlogLastLineNum-$lastLineNum))
+echo "Changed Line Size = $diffLine"
+if [ $diffLine -lt 0 ] ; then
+	echo "1" > $workPath/lastLineNum.txt 
+	sleep 1
+	echo "***---***"
+	echo "Line value reset."
+	echo "Next proccess will start from 1."
+	echo "***---***"
+	sleep 1
+fi
 
 echo "Cleaning files started...."
 > $workPath/ufwOut.txt
@@ -32,30 +42,30 @@ lineNum=$( wc -l $authlog | awk '{print $1}')
 echo $lineNum
 while IFS= read -r line
 do
- 
-userExist=$(echo "$line" | awk '{print $5}')
-#if user exist in machine
-if [ "$userExist" = "from" ]; then
-user=$(echo "$line" | awk '{print $4}')
-ip=$(echo "$line" | awk '{print $6}')
-
-#sudo ufw insert 1 deny from $ip to any >> $workPath/ufwOut.txt
-echo "$ip,$user" >> $workPath/listUserIp.txt
-echo "$ip" >> $workPath/listIp.txt
-fi
-
-if [ "$userExist" = "user" ]; then
-user=$(echo "$line" | awk '{print $6}')
-ip=$(echo "$line" | awk '{print $8}')
-
-#sudo ufw insert 1 deny from $ip to any >> $workPath/ufwOut.txt
-echo "$ip,$user" >> $workPath/listUserIp.txt
-echo "$ip" >> $workPath/listIp.txt
-fi
-i=$((i+1))
-result=$(((100*$i)/$lineNum))
-
-echo "\b\rPercentage $result%. File operations."
+	 
+	userExist=$(echo "$line" | awk '{print $5}')
+	#if user exist in machine
+	if [ "$userExist" = "from" ]; then
+		user=$(echo "$line" | awk '{print $4}')
+		ip=$(echo "$line" | awk '{print $6}')
+		
+		#sudo ufw insert 1 deny from $ip to any >> $workPath/ufwOut.txt
+		echo "$ip $user" >> $workPath/listUserIp.txt
+		echo "$ip" >> $workPath/listIp.txt
+	fi
+	
+	if [ "$userExist" = "user" ]; then
+		user=$(echo "$line" | awk '{print $6}')
+		ip=$(echo "$line" | awk '{print $8}')
+		
+		#sudo ufw insert 1 deny from $ip to any >> $workPath/ufwOut.txt
+		echo "$ip $user" >> $workPath/listUserIp.txt
+		echo "$ip" >> $workPath/listIp.txt
+	fi
+	i=$((i+1))
+	result=$(((100*$i)/$lineNum))
+	
+	echo "\b\rPercentage $result%. File operations."
 
 
 done < "$authlog"
@@ -70,9 +80,10 @@ sort $workPath/listIpUniq.txt > $workPath/listIpUniqTMP.txt
 mv $workPath/listIpUniqTMP.txt $workPath/listIpUniq.txt
 
 comm -2 -3 $workPath/listIpUniq.txt  $workPath/blockedIPs.txt | grep -v from | grep -v port > $workPath/diffIpList.txt
-
+lineNum=0
+i=0
 lineNum=$( wc -l $workPath/diffIpList.txt | awk '{print $1}')
-echo "\n"
+ 
 
 while IFS= read -r line
 do
@@ -80,9 +91,27 @@ do
 	i=$((i+1))
     result=$(((100*$i)/$lineNum))
 
-    echo "\b\b\r Percentage $result%. Blocked Ip for $line ...."
+    echo "\b\r Percentage $result%. Blocked Ip for $line ...."
 done < "$workPath/diffIpList.txt"
 
+lineNum=0
+i=0
+echo "Posting IPs to server."
+echo ""
+lineNum=$( wc -l $workPath/listUserIp.txt | awk '{print $1}')
+while IFS= read -r line
+do
+	 
+	ip=$(echo "$line" | awk '{print $1}')
+	user=$(echo "$line" | awk '{print $2}')
+	
+	result=$(curl -X POST -H "Content-Type: application/json" --data '{"ip":"'$ip'","user":"'$user'"}' http://108.61.135.97/ipblock/SetIp > /dev/null 2>&1 &)
+	i=$((i+1))
+    result=$(((100*$i)/$lineNum))
+    echo "\b\r Percentage $result%."
+done < "$workPath/listUserIp.txt"
+
+echo "Posting done."
 echo "Blocaked IPs value "
 sudo ufw status | wc -l
 echo "------------"
